da6551d7dc921bbc5067ea669e37cf665d81de78
first-coding <1690634875@qq.com>
2025-03-07 23:18:01 +0800

    delete

--- first/first/web/js/start.js
+++
@@ -1,4 +0,0 @@
-let data = decodeURI(location.search.substr('1', ).split('&')[0].split('=')[1])
-let commits = document.getElementById('last')
-let firsts = document.getElementById('first')
-firsts.innerHTML='欢迎您 , '+  data
